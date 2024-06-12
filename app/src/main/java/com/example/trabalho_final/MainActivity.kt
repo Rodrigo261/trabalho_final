138f532af8c0dcf6741ad9d44a341f1e0cbfa833
Rodrigo261 <goncalvesrodrigo097@gmail.com>
2024-06-12 21:54:38 +0100

    criaçao dos botoes e das respetivas paginas

--- app/src/main/java/com/example/trabalho_final/MainActivity.kt
+++ app/src/main/java/com/example/trabalho_final/MainActivity.kt
@@ -1,34 +1,24 @@
-package com.example.trabalhoFinal
+package com.example.trabalhofinal
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.trabalho_final.R
 
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonProteina = findViewById<Button>(R.id.button_proteina)
-        val buttonHidratos = findViewById<Button>(R.id.button_hidratos)
-        val buttonBebidas = findViewById<Button>(R.id.button_bebidas)
-        val buttonVegetais = findViewById<Button>(R.id.button_vegetais)
-        val buttonFrutas = findViewById<Button>(R.id.button_frutas)
-
-        buttonProteina.setOnClickListener {
-            val intent = Intent(this, ProteinaActivity::class.java)
+        buttonCarboidratos.setOnClickListener {
+            val intent = Intent(this, CarboidratosActivity::class.java)
             startActivity(intent)
         }
 
-        buttonHidratos.setOnClickListener {
-            val intent = Intent(this, HidratosActivity::class.java)
-            startActivity(intent)
-        }
-
-        buttonBebidas.setOnClickListener {
-            val intent = Intent(this, BebidasActivity::class.java)
+        buttonFruta.setOnClickListener {
+            val intent = Intent(this, FrutaActivity::class.java)
             startActivity(intent)
         }
 
@@ -37,13 +27,19 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        buttonFrutas.setOnClickListener {
-            val intent = Intent(this, FrutasActivity::class.java)
+        buttonCarne.setOnClickListener {
+            val intent = Intent(this, CarneActivity::class.java)
             startActivity(intent)
         }
-    }
 
-    private fun Intent(mainActivity: MainActivity, java: Any): Any {
-        TODO("Not yet implemented")
+        buttonPeixe.setOnClickListener {
+            val intent = Intent(this, PeixeActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonBebidas.setOnClickListener {
+            val intent = Intent(this, BebidasActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/trabalho_final/PeixeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.trabalhofinal
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.trabalho_final.R
+
+class PeixeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_peixe)
+    }
+}
