79ea2a2c2566303de52549070fdf2a34e834d110
Rodrigo261 <102800116+Rodrigo261@users.noreply.github.com>
2024-06-12 01:28:37 +0100

    criaçao botoes

--- app/src/main/java/com/example/trabalho_final/MainActivity.kt
+++ app/src/main/java/com/example/trabalho_final/MainActivity.kt
@@ -1,47 +1,49 @@
-package com.example.trabalho_final
+package com.example.trabalhoFinal
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.trabalho_final.ui.theme.Trabalho_finalTheme
+import android.widget.Button
+import com.example.trabalho_final.R
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Trabalho_finalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val buttonProteina = findViewById<Button>(R.id.button_proteina)
+        val buttonHidratos = findViewById<Button>(R.id.button_hidratos)
+        val buttonBebidas = findViewById<Button>(R.id.button_bebidas)
+        val buttonVegetais = findViewById<Button>(R.id.button_vegetais)
+        val buttonFrutas = findViewById<Button>(R.id.button_frutas)
+
+        buttonProteina.setOnClickListener {
+            val intent = Intent(this, ProteinaActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonHidratos.setOnClickListener {
+            val intent = Intent(this, HidratosActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonBebidas.setOnClickListener {
+            val intent = Intent(this, BebidasActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonVegetais.setOnClickListener {
+            val intent = Intent(this, VegetaisActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonFrutas.setOnClickListener {
+            val intent = Intent(this, FrutasActivity::class.java)
+            startActivity(intent)
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Trabalho_finalTheme {
-        Greeting("Android")
+    private fun Intent(mainActivity: MainActivity, java: Any): Any {
+        TODO("Not yet implemented")
     }
 }
