bacef77483f18ea42f3a70f5e33becac8955289c
Rodrigo261 <goncalvesrodrigo097@gmail.com>
2024-06-14 13:04:23 +0100

    criaçao do ObjetivoActivity

--- app/src/main/java/com/example/trabalho_final/MainActivity.kt
+++ app/src/main/java/com/example/trabalho_final/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.trabalho_final
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,35 +11,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         findViewById<View>(R.id.btnBebidas).setOnClickListener {
-
             startActivity(Intent(this, BebidasActivity::class.java))
         }
 
         findViewById<View>(R.id.btnPeixe).setOnClickListener {
-
             startActivity(Intent(this, PeixesActivity::class.java))
         }
 
         findViewById<View>(R.id.btnCarne).setOnClickListener {
-
             startActivity(Intent(this, CarnesActivity::class.java))
         }
 
         findViewById<View>(R.id.btnCarboidratos).setOnClickListener {
-
             startActivity(Intent(this, CarboidratosActivity::class.java))
         }
 
         findViewById<View>(R.id.btnFruta).setOnClickListener {
-
             startActivity(Intent(this, FrutaActivity::class.java))
         }
 
         findViewById<View>(R.id.btnVegetais).setOnClickListener {
-
             startActivity(Intent(this, VegetaisActivity::class.java))
+        }
+
+        findViewById<View>(R.id.btnObjetivo).setOnClickListener {
+            startActivity(Intent(this, ObjetivoActivity::class.java))
         }
     }
 }
